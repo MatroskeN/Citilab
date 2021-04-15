6ab42fc594b9cdb109f3e4dd9e4cb1d2a20bd91f
MatroskeN <44238649+MatroskeN@users.noreply.github.com>
2021-04-15 17:43:29 +0400

    а

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,17 @@
 $(document).ready(function (){
     var swiper = new Swiper('.banner-swiper', {
         slidesPerView: 'auto',
+        pagination: {
+            el: '.banner-pagination',
+            type: 'bullets',
+          },
       });
     var swiper1 = new Swiper('.popular-swiper', {
         slidesPerView: 'auto',
+        pagination: {
+            el: '.fraction-pagination',
+            type: 'fraction',
+        },
         navigation: {
             nextEl: '.next-arrow',
             prevEl: '.prew-arrow',
@@ -11,5 +19,21 @@ $(document).ready(function (){
     });
     var swiper2 = new Swiper('.review-swiper', {
         slidesPerView: 'auto',
+        pagination: {
+            el: '.fraction-pagination',
+            type: 'fraction',
+        },
+        navigation: {
+            nextEl: '.next-navigation',
+            prevEl: '.prev-navigation',
+          },
     });
+    var swiper3 = new Swiper('.mbanner-swiper', {
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        pagination: {
+            el: '.mbanner-pagination',
+            type: 'bullets',
+          },
+      });
 })
